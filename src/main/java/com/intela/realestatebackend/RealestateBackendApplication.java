b1453bece540027fcf129f54d374abf74fc55f82
Kudzai P Matizirofa <kudzaiprichard@gmail.comC:/Users/Kudzai Prichard/AppData/Local/Programs/Python/Python310-32/python.exe>
2023-07-08 01:18:04 +0200

    Initial Commit

---
+++ src/main/java/com/intela/realestatebackend/RealestateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.intela.realestatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealestateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealestateBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/intela/realestatebackend/RealestateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intela.realestatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealestateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
