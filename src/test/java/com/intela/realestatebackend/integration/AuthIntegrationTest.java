f89b881c9512d07f6ce584f4c22fbba38791493c
Kenneth Liu <kennethliu12138@gmail.com>
2024-08-22 22:09:49 +1000

    Test suite underway

---
+++ src/test/java/com/intela/realestatebackend/integration/AuthIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.intela.realestatebackend.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void registerCustomer() throws Exception {
+
+    }
+
+    @Test
+    void registerDealer() throws Exception {
+
+    }
+
+    @Test
+    void registerAdmin() throws Exception {
+
+    }
+
+}
